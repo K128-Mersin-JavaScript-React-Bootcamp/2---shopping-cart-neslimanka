a34fe30d669c9e821404b35c5ce0a3f2d77866be
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-10-08 21:34:27 +0300

    Home Page yazıldı

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,14 +1,34 @@
-
 import React, { Component } from "react";
-
+import { Container } from "@material-ui/core";
+import Slider from "../Slider";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from '@mui/material/Button';
 export default class Home extends Component {
-
-render(){
+  render() {
     return (
-        <div>
-            <h3>This is Home Component</h3>
-        </div>
-    )
-}
+      <div>
+        <Typography component="div">
+          <Box sx={{ textAlign: "center", m: 3 }}>
+            Better clothing for the planet
+            Create screens directly in Method or add ypur images from Sketch or Figma.
+          </Box>
 
+          <Box sx={{ textAlign: "center", m: 3}}>
+            You can even sync designs from your cloud storage!
+            
+            </Box>
+            <Box sx={{ textAlign: "center", m: 3}}>
+            <Button variant="contained" color="secondary" size="large"  href="/shop">
+                   SHOP
+               </Button>
+          
+            </Box>
+        </Typography>
+        <Container>
+          <Slider></Slider>
+        </Container>
+      </div>
+    );
+  }
 }
---
+++ src/components/Slider.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { UncontrolledCarousel } from 'reactstrap';
+import alisveris2 from "../img/alisveris2.png";
+import alisveris3 from "../img/alisveris3.png";
+
+
+
+const items = [
+  { 
+    src: alisveris3,
+    altText: 'Slide 1',
+    caption: 'Slide 1',
+    header: 'Slide 1 Header',
+    key: '1'
+  },
+  {
+    src: alisveris2,
+    altText: 'Slide 2',
+    caption: 'Slide 2',
+    header: 'Slide 2 Header',
+    key: '2'
+  },
+  {
+    src: alisveris3,
+    altText: 'Slide 3',
+    caption: 'Slide 3',
+    header: 'Slide 3 Header',
+    key: '3'
+  }
+];
+
+const Example = () => <UncontrolledCarousel items={items}  />;
+
+export default Example;
